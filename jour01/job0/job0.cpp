835090624e43c555500e851052be671e56d48cb0
samantha-colmart <samantha.colmart@laplateforme.io>
2026-09-01 09:07:45 +0200

    first commit

---
+++ jour01/job0/job0.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int x = 5;
+    int y = 10;
+    int b = 3;
+    int c = 4;
+    int d = 8;
+    int i = 2;
+    int n = 3;
+    int p = 4;
+
+    a = x + 5;
+    a = x = y + 2;
+    a = x == y;
+    a < b && c < d;
+    i++ * (n + p);
+
+    return 0;
+}
